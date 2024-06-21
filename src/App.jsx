cf190ac43a078503ebbafdeb729ce158912dc2ed
rafiftrlh <rafifitro89@gmail.com>
2024-06-21 19:25:29 +0700

    view: create sidebar

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Sidebar from "./components/Sidebar";
+
+const App = () => {
+  return (
+    <div className="h-screen bg-black">
+      <div className="h-[90%] flex">
+        <Sidebar />
+      </div>
+    </div>
+  );
+};
+
+export default App;
